f81d0ee18da0f4c3138ad731c2839ba0defc7526
AkashBehara <behara.akash68@gmail.com>
2024-03-21 13:39:32 +0530

    SD UI update

--- index.js
+++ index.js
@@ -22,16 +22,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     const listItems = document.querySelectorAll('.Bullet__point');
+    const listItemsdots = document.querySelectorAll('.Bullet__Dot');
     function colorItems() {
         let index = 0;
+        let dotIdx = 0;
         return function() {
         // Remove the color from all items
         listItems.forEach(item => item.classList.remove('Bullet_Point_Active'));
+        listItemsdots.forEach(item => item.classList.remove('Bullet__Dot__Active'));
         
         // Color the current item
         listItems[index].classList.add('Bullet_Point_Active');
+        listItemsdots[dotIdx].classList.add('Bullet__Dot__Active');
         
         // Move to the next item
+        dotIdx = (dotIdx + 1) % listItemsdots.length;
         index = (index + 1) % listItems.length;
         };
     }
